181a726d7a60ac2c7cab7b37f32d3e5737837b4e
Ahmed.Elsayed <ml.ahmed1187@gmail.com>
2024-08-27 09:40:01 +0300

    create a project

---
+++ RealExperience/src/main/java/com/corevision/experience/RealExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.corevision.experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealExperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealExperienceApplication.class, args);
+	}
+
+}
